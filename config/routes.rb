5af95ffc272c92fb7ab65a3450d0f10d9e3c81d1
ashutoshcp <as01hu@gmail.com>
2020-01-02 10:40:52 +0530

    active admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
