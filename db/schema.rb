52fba0026cd3b3ec1a39ca0d4b2b146101416346
ashutoshcp <as01hu@gmail.com>
2020-02-02 03:01:43 +0530

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20200131045453) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", ˚unique: true
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
